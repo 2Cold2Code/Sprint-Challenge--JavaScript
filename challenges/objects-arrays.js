234bbb568215671a0a043360b61f5e0bbf88c29b
2Cold2Code <wizardofosburn@gmail.com>
2020-01-24 20:07:00 -0500

    completed zoo request 2

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -134,7 +134,7 @@ The zoos need a list of all their animal's names (animal_name only) converted to
 
 */
 
-const lowCaseAnimalNames = [];
+const lowCaseAnimalNames = zooAnimals.map(x => x.animal_name.toLowerCase());
 console.log(lowCaseAnimalNames);
 
 /* Request 3: .filter() 
