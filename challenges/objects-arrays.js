ab011099f74c3bdc5c84e31623ce7055b37b0cc9
2Cold2Code <wizardofosburn@gmail.com>
2020-01-24 20:02:48 -0500

    request 3 complete

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -38,19 +38,24 @@ const velociraptor = {
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
+
 console.log(tyrannosaurus.weight);
 
 // What was the diet of a velociraptor?
+
 console.log(velociraptor.weight);
 
 // How long was a stegosaurus?
+
 console.log(stegosaurus.length);
 
 // What time period did tyrannosaurus live in?
+
 console.log(tyrannosaurus.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
+
 const tyrannosaurus.roar = () => "RAWERSRARARWERSARARARRRR!";
 console.log(tyrannosaurus.roar());
 
@@ -76,6 +81,7 @@ const graduates = [
 /* Request 1: Create a new array called universities that contains all the universities in the graduates array. This will be an array of strings.
 
 Once you have the new array created, sort the universities alphabetically and log the result. */
+
 const universities = graduates.map(x => x.university);
 universities.sort((a, b) => a.toUpperCase() > b.toUpperCaseO() ? 1 : a.toUpperCase() < b.toUpperCase() ? -1 : 0);
 console.log(universities);
@@ -86,11 +92,13 @@ The resulting contact information strings should have a space between the first 
 "Josh josh@example.com"
 
 Log the result of your new array. */
+
 const contactInfo = `${graduates.first_name} ${graduates.email}`;
 console.log(contactInfo);
 
 /* Request 3: Find out how many universities have the string "Uni" included in their name. Create a new array called unisWithUni that contains them all. This will be an array of objects. Log the result. */
-const unisWithUni = [];
+
+const unisWithUni = graduates.filter(x => x.university.includes('Uni'));
 console.log(unisWithUni);
 
 
