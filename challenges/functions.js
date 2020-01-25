9e460dc45be921774c2a922efdc6f75e3e3fd5fc
2Cold2Code <wizardofosburn@gmail.com>
2020-01-24 19:38:30 -0500

    step 3 complete

--- challenges/functions.js
+++ challenges/functions.js
@@ -20,9 +20,9 @@ const greeting = (firstName, lastName) => `Hello ${firstName} ${lastName}, nice 
 
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// console.log(consume(2, 2, add)); // 4
-// console.log(consume(10, 16, multiply)); // 160
-// console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
+console.log(consume(2, 2, add)); // 4
+console.log(consume(10, 16, multiply)); // 160
+console.log(consume("Mary", "Poppins", greeting)); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
